228d959f6d28dfb6fb5bb3058f6f122d8ea5b01c
pranjul jangra <pranjuljan68@gmail.com>
2025-09-02 09:53:07 +0530

    fix add button color issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar({ isLightTheme }) {
             <h1 className='text-3xl font-bold tracking-wide'>Expensely</h1>
 
             <div className="flex items-center *:not-focus:outline-none *:font-semibold *:cursor-pointer space-x-3">
-                <button type="button" aria-label="Add transaction" className={`sm:bg-blue-500 sm:hover:bg-blue-600 text-white border ${borderColor} sm:border-none p-2 rounded-full sm:rounded-lg flex items-center gap-1 transition-colors duration-150`} onClick={() => navigate("/home/add-transaction")}>
+                <button type="button" aria-label="Add transaction" className={`sm:bg-blue-500 sm:hover:bg-blue-600 sm:text-white border ${borderColor} sm:border-none p-2 rounded-full sm:rounded-lg flex items-center gap-1 transition-colors duration-150`} onClick={() => navigate("/home/add-transaction")}>
                     <IoAdd className="text-xl font-bold shrink-0" />
                     <span className="max-sm:hidden">Add Transaction</span>
                 </button>
